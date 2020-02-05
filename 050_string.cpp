b460e3f9bbe5081f4dd7d727c5d72ae1192216be
weikaisu <37170449+weikaisu@users.noreply.github.com>
2020-02-05 16:23:36 +0800

    Update lc0014

--- 050_string.cpp
+++ 050_string.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 //------------------------------------------------------------------------------------------------- Easy
 
+/*
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ */
+struct lc0014 {
+    string longestCommonPrefix(vector<string>& strs) {
+    string ans {};
+        if(strs.empty()) return ans;
+    string s = strs.front();
+    int i=0;
+        for(auto chr:s) {
+            for(auto str:strs)
+                if (chr != str[i]) return ans;
+            i++;
+            ans += chr;
+        }
+    return ans;
+    }
+};
+
+void main_lc0014(void)
+{
+    lc0014 solu;
+    vector<string> strs {"flower","flow","flight"};
+    for(auto str:strs)
+        cout << str << " ";
+    auto ans = solu.longestCommonPrefix(strs);
+    cout << endl << ans << endl;
+}
+
+/*
+ * Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+ */
 #include <unordered_map>
 struct lc0013 {
     int romanToInt(string s) {
