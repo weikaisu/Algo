efea9102a6e71e213b2dc4dcea65a9eae23795e5
weikaisu <37170449+weikaisu@users.noreply.github.com>
2020-03-10 10:32:59 +0800

    Update lc0121

--- 001_list.cpp
+++ 001_list.cpp
@@ -67,6 +67,31 @@ void main_lc0011(void)
 //------------------------------------------------------------------------------------------------- Easy
 
 /*
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock),
+ * design an algorithm to find the maximum profit.
+ * Note that you cannot sell a stock before you buy one.
+ */
+struct lc0121 {
+    int maxProfit(vector<int>& prices) {
+        int buy = INT_MIN * -1;
+        int prf = 0;
+        for(auto price:prices) {
+            buy = max(buy, price * -1);
+            prf = max(prf, price+buy);
+        }
+        return prf;
+    }
+};
+
+void main_lc0121() {
+    lc0121 solu;
+    vector<int> prices {7,1,5,3,6,4};
+    cout << solu.maxProfit(prices);
+}
+
+
+/*
  * Given a sorted linked list, delete all duplicates such that each element appear only once.
  */
 #include <unordered_set>
