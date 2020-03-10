08b8fb4a4d2291382d4710ed1f51d061342f3f68
weikaisu <37170449+weikaisu@users.noreply.github.com>
2020-03-10 10:53:51 +0800

    Update lc0122

--- 001_list.cpp
+++ 001_list.cpp
@@ -68,6 +68,28 @@ void main_lc0011(void)
 
 /*
  * Say you have an array for which the ith element is the price of a given stock on day i.
+ * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and
+ * sell one share of the stock multiple times).
+ * Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+ */
+struct lc0122 {
+    int maxProfit(vector<int>& prices) {
+        int prf = 0;
+        for(auto i=1; i<prices.size(); i++)
+            prf += max(prices[i]-prices[i-1], 0);
+        return prf;
+    }
+};
+
+void main_lc0122() {
+    lc0122 solu;
+    vector<int> prices {7,1,5,3,6,4};
+    cout << solu.maxProfit(prices);
+}
+
+
+/*
+ * Say you have an array for which the ith element is the price of a given stock on day i.
  * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock),
  * design an algorithm to find the maximum profit.
  * Note that you cannot sell a stock before you buy one.
